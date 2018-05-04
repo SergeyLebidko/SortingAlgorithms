50c21a096a89fb5ddd2a2998e6dfebd72083f8ab
SergeyLebidko <it.sergeyler@mail.ru>
2018-05-04 23:19:36 +0300

    Добавлена сортировка выбором и сортировка Шелла

--- src/sortingalgorithms/MainClass.java
+++ src/sortingalgorithms/MainClass.java
@@ -4,40 +4,77 @@ import java.util.Arrays;
 
 public class MainClass {
 
-    private static final int SIZE_ARRAY=100;
+    private static final int SIZE_ARRAY = 100;
 
     //Сортировка пузырьком
-    private static <E extends Comparable<E>> E[] sortBubble(E[] e){
+    private static <E extends Comparable<E>> E[] sortBubble(E[] e) {
         if (e == null) return null;
 
-        int compareCount=0;        //Счетчик сравнений
-        int assignCount=0;         //Счетчик присваиваний
+        int compareCount = 0;        //Счетчик сравнений
+        int assignCount = 0;         //Счетчик присваиваний
 
-        boolean bitSwap=false;     //Равен true, если в результате прохода по массиву была выполнена хоть одна перестановка
+        boolean bitSwap = false;     //Равен true, если в результате прохода по массиву была выполнена хоть одна перестановка
         E tmp;                     //Переменная для временного хранения перемещаемых элементов
 
-        for (int i=(e.length-1);i>0;i--){
-            bitSwap=false;
-            for (int j=1;j<=i;j++){
-                if (e[j-1].compareTo(e[j])==1){
+        for (int i = (e.length - 1); i > 0; i--) {
+            bitSwap = false;
+            for (int j = 1; j <= i; j++) {
+                if (e[j - 1].compareTo(e[j]) == 1) {
                     compareCount++;
-                    tmp=e[j-1];
-                    e[j-1]=e[j];
-                    e[j]=tmp;
-                    assignCount+=3;
-                    bitSwap=true;
+                    tmp = e[j - 1];
+                    e[j - 1] = e[j];
+                    e[j] = tmp;
+                    assignCount += 3;
+                    bitSwap = true;
                     continue;
                 }
                 compareCount++;
             }
-            if (!bitSwap)break;
+            if (!bitSwap) break;
         }
 
         //Выводим статистику по работе алгоритма
         System.out.println();
         System.out.println("Сортировка пузырьком");
-        System.out.println("    выполнено сравнений: "+compareCount);
-        System.out.println("    выполнено присваиваний: "+assignCount);
+        System.out.println("    выполнено сравнений: " + compareCount);
+        System.out.println("    выполнено присваиваний: " + assignCount);
+
+        return e;
+    }
+
+    //Сортировка методом выбора
+    private static <E extends Comparable<E>> E[] sortSelect(E[] e) {
+        if (e == null) return null;
+
+        int compareCount = 0;        //Счетчик сравнений
+        int assignCount = 0;         //Счетчик присваиваний
+
+        int posMin;                //Указатель на элемент с минимальным значением
+        E tmp;                     //Переменная для временного хранения перемещаемых элементов
+
+        for (int i = 0; i < (e.length - 1); i++) {
+            posMin = i;
+            for (int j = (i + 1); j < e.length; j++) {
+                if (e[j].compareTo(e[posMin]) == (-1)) {
+                    compareCount++;
+                    posMin = j;
+                    continue;
+                }
+                compareCount++;
+            }
+            if (posMin != i) {
+                tmp = e[i];
+                e[i] = e[posMin];
+                e[posMin] = tmp;
+                assignCount += 3;
+            }
+        }
+
+        //Выводим статистику по работе алгоритма
+        System.out.println();
+        System.out.println("Сортировка методом выбора");
+        System.out.println("    выполнено сравнений: " + compareCount);
+        System.out.println("    выполнено присваиваний: " + assignCount);
 
         return e;
     }
@@ -46,8 +83,8 @@ public class MainClass {
     private static <E extends Comparable<E>> E[] sortInsert(E[] e) {
         if (e == null) return null;
 
-        int compareCount=0;        //Счетчик сравнений
-        int assignCount=0;         //Счетчик присваиваний
+        int compareCount = 0;        //Счетчик сравнений
+        int assignCount = 0;         //Счетчик присваиваний
 
         int sizeSortedArea = 1;    //Первоначальный размер отсортированной области
         E tmp;                     //Переменная для временного хранения перемещаемых элементов
@@ -73,8 +110,59 @@ public class MainClass {
         //Выводим статистику по работе алгоритма
         System.out.println();
         System.out.println("Сортировка вставками");
-        System.out.println("    выполнено сравнений: "+compareCount);
-        System.out.println("    выполнено присваиваний: "+assignCount);
+        System.out.println("    выполнено сравнений: " + compareCount);
+        System.out.println("    выполнено присваиваний: " + assignCount);
+
+        return e;
+    }
+
+    //Сортировка Шелла
+    private static <E extends Comparable<E>> E[] sortShell(E[] e) {
+        if (e == null) return null;
+
+        int compareCount = 0;        //Счетчик сравнений
+        int assignCount = 0;         //Счетчик присваиваний
+
+        E tmp;                       //Переменная для временного хранения перемещаемых элементов
+        int k;                       //Вспомогательная переменная-счетчик
+
+        int h=1;                     //Начальное значение интервала
+        while (h<=(e.length/3)){
+            h=h*3+1;
+        }
+
+        //В самом внешнем цикле перебираем значения интервала
+        while (h>0){
+
+            //В этом цикле сдвигаем "интервальную рамку"
+            for (int i=0;i<h;i++){
+
+                //В этом цикле сортируем элементы в пределах одного интервала
+                for (int j=(i+h);j<e.length;j+=h){
+                    tmp=e[j];
+                    assignCount++;
+                    k=j-h;
+                    while (k>=0 && tmp.compareTo(e[k])==-1){
+                        compareCount++;
+                        e[k+h]=e[k];
+                        assignCount++;
+                        k-=h;
+                    }
+                    compareCount++;
+                    e[k+h]=tmp;
+                    assignCount++;
+                }
+
+            }
+
+            h=(h-1)/3;
+        }
+
+        //Выводим статистику по работе алгоритма
+        System.out.println();
+        System.out.println("Сортировка Шелла");
+        System.out.println("    выполнено сравнений: " + compareCount);
+        System.out.println("    выполнено присваиваний: " + assignCount);
 
         return e;
     }
@@ -90,26 +178,34 @@ public class MainClass {
     }
 
     public static void main(String[] args) {
-        Integer[] a0=new Integer[SIZE_ARRAY];   //Исходный массив
-        Integer[] a1=new Integer[SIZE_ARRAY];   //Массив для сортировки пузырьком
-        Integer[] a2=new Integer[SIZE_ARRAY];   //Массив для сортировки выбором
-        Integer[] a3=new Integer[SIZE_ARRAY];   //Массив для сортировки вставками
+        Integer[] a0 = new Integer[SIZE_ARRAY];   //Исходный массив
+        Integer[] a1 = new Integer[SIZE_ARRAY];   //Массив для сортировки пузырьком
+        Integer[] a2 = new Integer[SIZE_ARRAY];   //Массив для сортировки выбором
+        Integer[] a3 = new Integer[SIZE_ARRAY];   //Массив для сортировки вставками
+        Integer[] a4 = new Integer[SIZE_ARRAY];   //Массив для сортировки Шелла
 
         //Заполняем и выводим исходный массив
-        for (int i=0;i<SIZE_ARRAY;i++)a0[i]=new Integer((int) (Math.random()*SIZE_ARRAY));
+        for (int i = 0; i < SIZE_ARRAY; i++) a0[i] = new Integer((int) (Math.random() * SIZE_ARRAY));
         System.out.println("Исходный числовой массив (целые числа):");
         show(a0);
 
         //Заполняем вспомогательные массивы, которые будем потом сортировать
-        a1=Arrays.copyOf(a0,SIZE_ARRAY);
-        a2=Arrays.copyOf(a0,SIZE_ARRAY);
-        a3=Arrays.copyOf(a0,SIZE_ARRAY);
+        a1 = Arrays.copyOf(a0, SIZE_ARRAY);
+        a2 = Arrays.copyOf(a0, SIZE_ARRAY);
+        a3 = Arrays.copyOf(a0, SIZE_ARRAY);
+        a4 = Arrays.copyOf(a0, SIZE_ARRAY);
 
         //Сортировка пузырьком
-        a1=sortBubble(a1);
+        a1 = sortBubble(a1);
+
+        //Сортировка методом выбора
+        a2 = sortSelect(a2);
 
         //Сортировка вставками
-        a3= sortInsert(a3);
+        a3 = sortInsert(a3);
+
+        //Сортировка Шелла
+        a4 = sortShell(a4);
 
     }
 
